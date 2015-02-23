17a714369e6b0e460d9790299d654f48ef6810cb
Francisco Trindade <francisco.alexandre.trindade@gmail.com>
2015-02-23 01:09:15 +0000

    Final

--- xicojs/app.js
+++ xicojs/app.js
@@ -7,22 +7,48 @@ var myApp = angular.module('myApp',[]);
 
 
 
-myApp.controller('mainController',function($scope){
+myApp.controller('mainController',function($scope,$window){
 	$scope.teste='Hello World';
 
 	$scope.link1='Home'; 
 	$scope.link2='Introduction';
 	$scope.link3='AngularJS';
 	$scope.link4='Code';
-	$scope.link7='About me'; 
+	$scope.link7='About us'; 
 
 
 
-	//console.log(searchPeople(1,2,3,4));
+    $scope.todos = [
+        {text:'Learn AngularJS', done:false},
+        {text:'Build an app', done:false}
+    ];
+
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    };
+
+    $scope.clearCompleted = function () {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    };
+
+    $scope.addTodo = function () {
+        $scope.todos.push({text:$scope.formTodoText, done:false});
+        $scope.formTodoText = '';
+    };
+
+	$scope.openWebsite = function() {
+    	$window.open($scope.link);
+  	};
+
 });
 
 
 
+//model
 var searchPeople = function (firstName, lastName, age, occupation){
-    return 'Francisco Trindade';
-}
+    return 'Francisco Trindade';};
+
+
+
